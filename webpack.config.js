6bfcf734510c1f18a364bc84f8a324e24564cc2d
Carter Rink <carter.m.rink@gmail.com>
2019-08-18 21:12:23 -0400

    path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: ['@babel/polyfill', './src/js/index.js'], //where webpack will start the bundling
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: '/',
         filename: 'js/bundle.js'
     },
     devServer: {
